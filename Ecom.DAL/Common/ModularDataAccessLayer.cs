c063995934e03e7cddb42d8275c60d2a3466b254
FadyTawadrous <40478872+FadyTawadrous@users.noreply.github.com>
2025-11-03 13:40:13 +0200

    Added folder structure for DAL

---
+++ Ecom.DAL/Common/ModularDataAccessLayer.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+
+namespace Ecom.DAL.Common
+{
+    public static class ModularDataAccessLayer
+    {
+        
+    }
+}
---
+++ Ecom.DAL/Global/Global.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.IdentityModel.Tokens;
+global using Ecom.DAL.Entity;
+global using Ecom.DAL.Database;
+global using System.Linq.Expressions;
